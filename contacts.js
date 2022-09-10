3d0f83fcc179ab8af95a7cb57a561ba34830b4d2
Vadym Frolov <vdfroloff@gmail.com>
2022-09-10 12:07:06 +0300

    upds

--- contacts.js
+++ contacts.js
@@ -1,24 +1,47 @@
 const fs = require('fs').promises;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-// TODO: задокументировать каждую функцию
 function listContacts() {
-  // ...твой код
+  return JSON.parse(fs.readFile(contactsPath, 'utf8'));
+  // const contacts = JSON.parse(fs.readFile(contactsPath, 'utf8'));
+  // console.table(contacts);
 }
 
 function getContactById(contactId) {
-  // ...твой код
+  const allContacts = listContacts();
+  const contactToFind = allContacts.findIndex(contact => contact.id === contactId.toString());
+
+  if (contactToFind === -1) {
+    return `there is no contact with id: ${contactId}`;
+  }
+  return contactToFind;
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  const allContacts = listContacts();
+  try {
+    const newContactsList = JSON.stringify(
+      allContacts.filter(contact => contact.id !== contactId.toString())
+    );
+    await fs.writeFile(contactsPath, newContactsList);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  const allContacts = listContacts();
+  const newContact = { id: uuidv4(), name, email, phone };
+  try {
+    const newContactsList = JSON.stringify([...allContacts, newContact]);
+    await fs.writeFile(contactsPath, newContactsList);
+    return newContactsList;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+module.exports = { listContacts, addContact, getContactById, removeContact };
